c721ff84b19e9d54a5539437c94d9847fee19fcf
Molly <mfmsproul@gmail.com>
2020-11-01 23:48:04 -0500

    done 02

--- 02-sockets/src/client.cpp
+++ 02-sockets/src/client.cpp
@@ -4,22 +4,79 @@
 using asio::ip::tcp;
 
 int main() {
-  asio::io_context io_context;
-  tcp::resolver resolver(io_context);
-  tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "3000");
-
-  tcp::socket socket(io_context);
-  asio::connect(socket, endpoints);
-
-  asio::error_code error;
-  std::array<uint8_t, 3> buf;
-  buf.fill(0);
-  buf[0] |= 1 << 7;
-  asio::write(socket, asio::buffer(buf), error);
-
-  size_t len = socket.read_some(asio::buffer(buf), error);
-  uint16_t response = *reinterpret_cast<uint16_t*>(&buf.data()[0]);
-  std::cout << response << std::endl;
-
-  return 0;
+    asio::io_context io_context;
+    tcp::resolver resolver(io_context);
+    tcp::resolver::results_type endpoints = resolver.resolve("127.0.0.1", "3000");
+    tcp::socket socket(io_context);
+    asio::error_code error;
+    
+    // • Get the number of orders for E=01 (Claudia)
+    // M = 01
+    // E = 01
+    // Q1 = 0000
+    // Q2 = 0000
+    // Q3 = 0000
+    // Q4 = 0000
+    // Q5 = 0000
+    std::cout << "Get the number of orders for E=01 (Claudia)" << std::endl;
+    std::array<uint8_t, 3> buf = {0b01010000, 0b00000000, 0b00000000};
+    
+    asio::connect(socket, endpoints);
+    asio::write(socket, asio::buffer(buf), error);
+    size_t len = socket.read_some(asio::buffer(buf), error);
+    uint16_t response = *reinterpret_cast<uint16_t*>(&buf.data()[0]);
+    std::cout << "Response: " << response << std::endl;
+    
+    // • Add an order: E=01, Q1=2, Q2=5, Q3=0, Q4=1, Q5=0
+    // M = 10
+    // E = 01
+    // Q1 = 0010
+    // Q2 = 0101
+    // Q3 = 0000
+    // Q4 = 0001
+    // Q5 = 0000
+    std::cout << "Add an order: E=01, Q1=2, Q2=5, Q3=0, Q4=1, Q5=0" << std::endl;
+    buf = {0b10010010, 0b01010000, 0b00010000};
+    
+    asio::connect(socket, endpoints);
+    asio::write(socket, asio::buffer(buf), error);
+    len = socket.read_some(asio::buffer(buf), error);
+    response = *reinterpret_cast<uint16_t*>(&buf.data()[0]);
+    std::cout << "Response: " << response << std::endl;
+    
+    // • Get the number of orders for E=01 (should have increased by one!)
+    // M = 01
+    // E = 01
+    // Q1 = 0000
+    // Q2 = 0000
+    // Q3 = 0000
+    // Q4 = 0000
+    // Q5 = 0000
+    std::cout << "Get the number of orders for E=01 (should have increased by one!)" << std::endl;
+    buf = {0b01010000, 0b00000000, 0b00000000};
+    
+    asio::connect(socket, endpoints);
+    asio::write(socket, asio::buffer(buf), error);
+    len = socket.read_some(asio::buffer(buf), error);
+    response = *reinterpret_cast<uint16_t*>(&buf.data()[0]);
+    std::cout << "Response: " << response << std::endl;
+    
+    // • Get the total number of orders
+    // M = 00
+    // E = 00
+    // Q1 = 0000
+    // Q2 = 0000
+    // Q3 = 0000
+    // Q4 = 0000
+    // Q5 = 0000
+    std::cout << "Get the total number of orders" << std::endl;
+    buf = {0b00000000, 0b00000000, 0b00000000};
+    
+    asio::connect(socket, endpoints);
+    asio::write(socket, asio::buffer(buf), error);
+    len = socket.read_some(asio::buffer(buf), error);
+    response = *reinterpret_cast<uint16_t*>(&buf.data()[0]);
+    std::cout << "Response: " << response << std::endl;
+    
+    return 0;
 }
--- 02-sockets/src/server.cpp
+++ 02-sockets/src/server.cpp
@@ -7,41 +7,141 @@
 #include "bakery.hpp"
 
 using asio::ip::tcp;
+int get_response(std::array<uint8_t, 3> buf, Bakery& bakery);
 
 int main() {
-  asio::io_context io_context;
-  tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 3000));
+    asio::io_context io_context;
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 3000));
+    
+    // Use this bakery to handle queries from the client
+    Bakery bakery = text_deserializer("../data/bakery.txt");
+    
+    while (true) {
+        // Wait for client
+        std::cout << "Blocked for read" << std::endl;
+        tcp::socket socket(io_context);
+        acceptor.accept(socket);
+        std::array<uint8_t, 3> buf;
+        asio::error_code error;
+        size_t len = socket.read_some(asio::buffer(buf), error);
+        // Example of error handling
+        // if (error != asio::error::eof)
+        //   throw asio::system_error(error);
+        
+        // last step, sends response back
+        //std::cout << "Response: " << response << std::endl;
+        int response = get_response(buf, bakery);
+        std::cout<< "Response: " << response << std::endl;
+        
+        std::array<uint8_t, 2> return_buf;
+        return_buf.fill(0);
+        if (response > 255){
+            return_buf[0] = 255;
+            return_buf[1] = (response - 255);
+        }
+        else {
+            return_buf[0] = response;
+        }
+        
+        std::memcpy(&buf, &return_buf, sizeof(uint16_t));
+        asio::write(socket, asio::buffer(buf), error);
+    }
+    
+    return 0;
+}
 
-  // Use this bakery to handle queries from the client
-  Bakery bakery = text_deserializer("../data/bakery.txt");
-
-  uint16_t counter = 0;
-
-  while (true) {
-    // Wait for client
-    std::cout << "Blocked for read" << std::endl;
-    tcp::socket socket(io_context);
-    acceptor.accept(socket);
-
-    std::array<uint8_t, 3> buf;
-    asio::error_code error;
-    size_t len = socket.read_some(asio::buffer(buf), error);
-
-    // Example of error handling
-    // if (error != asio::error::eof)
-    //   throw asio::system_error(error);
-
-    // Add x to counter
-    auto x = uint8_t(buf[0]);
-    counter += x;
-    std::cout << +x << " " << counter << std::endl;
-
-    buf.fill(0);
-
-    std::memcpy(&buf, &counter, sizeof(uint16_t));
-
-    asio::write(socket, asio::buffer(buf), error);
-  }
-
-  return 0;
+int get_response(std::array<uint8_t, 3> buf, Bakery& bakery)
+{
+    // to get action, shift 6
+    uint8_t m_value = buf[0]>>6;
+    
+    if (m_value == 0b00) {
+        // if M = 00, get total number of orders
+        return bakery.orders.size();
+    }
+    else if (m_value == 0b01) {
+        // if M = 01, get number of orders for employee E
+        
+        uint8_t employee_bits = buf[0] >> 4;
+        std::string employee_name;
+        
+        // find E
+        if (employee_bits == 0b0100) {
+            employee_name = "Brad";
+        }
+        else if (employee_bits == 0b0101) {
+            employee_name = "Claudia";
+        }
+        else if (employee_bits == 0b0110) {
+            employee_name = "Simone";
+        }
+        else if (employee_bits == 0b0111) {
+            return 0;
+        }
+        
+        int count = 0;
+        for (Order temp_order : bakery.orders) {
+            if (temp_order.employee == employee_name) {
+                count ++;
+            }
+        }
+        return count;
+    }
+    
+    else if (m_value == 0b10){
+        // if M = 10, add an order with E, Q1, Q2, ..., Q5 values
+        Order temp_order;
+        
+        uint8_t employee_bits = buf[0] >> 4;
+        
+        if (employee_bits == 0b1000) {
+            temp_order.employee = "Brad";
+        }
+        else if (employee_bits == 0b1001){
+            temp_order.employee = "Claudia";
+        }
+        else if (employee_bits == 0b1010){
+            temp_order.employee = "Simone";
+        }
+        else if (employee_bits == 0b1011){
+            return 0;
+        }
+        
+        // get quantities of all the products
+        uint8_t q_1 = buf[0] & 00001111;
+        uint8_t q_2 = buf[1] >> 4;
+        uint8_t q_3 = buf[1] & 0b00001111;
+        uint8_t q_4 = buf[2] >> 4;
+        uint8_t q_5 = buf[2] & 0b00001111;
+        
+        if (not q_1) {
+            std::string temp_string = std::to_string(q_1);
+            temp_order.items.push_back(std::make_pair("Biscuit", temp_string));
+        }
+        
+        if (not q_2){
+            std::string temp_string = std::to_string(q_2);
+            temp_order.items.push_back(std::make_pair("Bun", temp_string));
+        }
+        
+        if (not q_3) {
+            std::string temp_string = std::to_string(q_3);
+            temp_order.items.push_back(std::make_pair("Brownie", temp_string));
+        }
+        
+        if (not q_4) {
+            std::string temp_string = std::to_string(q_4);
+            temp_order.items.push_back(std::make_pair("White Loaf", temp_string));
+        }
+        if (not q_5) {
+            std::string temp_string = std::to_string(q_5);
+            temp_order.items.push_back(std::make_pair("Wheat Loaf", temp_string));
+        }
+        
+        bakery.orders.push_back(temp_order);
+        return 0;
+    }
+    
+    return 0;
+    
 }
